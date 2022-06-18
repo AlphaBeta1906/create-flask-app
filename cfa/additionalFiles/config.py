2a1c60006f4bd4121a0caab5dd404403fc35222e
fariz <farizi1906@gmail.com>
2022-06-18 13:20:16 +0700

    update and edit additional files

--- cfa/additionalFiles/config.py
+++ cfa/additionalFiles/config.py
@@ -5,15 +5,6 @@ class Config(object):
     SECRET_KEY = "{{generate_key(50)}}"
     SECURITY_PASSWORD_SALT = "{{generate_key(50)}}"
     
-    {% if database == "flask-jwt-extended"%} 
-    JWT_SECRET_KEY = "{{generate_key(50)}}"
-    {% endif%}
-    
-    {% if database == "mysql"%}
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://user:pass@host/database"
-    {% elif database == "postgresql"%}
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://user:pass@host:port/database"
-    {% else %}
-    SQLALCHEMY_DATABASE_URI = "sqlite://dbase.db"
-    {% endif %}
+    {% if database == "flask-jwt-extended"%} JWT_SECRET_KEY = "{{generate_key(50)}}"{% endif%}    
+    {% if database == "mysql"%}SQLALCHEMY_DATABASE_URI = "mysql+pymysql://user:pass@host/database"{% elif database == "postgresql"%}SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://user:pass@host:port/database"{% else %}SQLALCHEMY_DATABASE_URI = "sqlite://dbase.db"{% endif %}
     SQLALCHEMY_TRACK_MODIFICATIONS = False
