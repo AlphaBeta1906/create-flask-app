0c9ac10b4bb47f847daf07eda7137748f39474c1
fariz <farizi1906@gmail.com>
2022-07-06 14:10:09 +0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup,find_packages
 
 setup(
-    name="flask-create-project",
+    name="flask-create-app",
     version="0.1.1",
     description="a simple cli tools to generate template for flask app",
     long_description= open("README.md","r").read(),
