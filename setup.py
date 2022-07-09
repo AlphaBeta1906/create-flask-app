dd5d415337b60cabf13917dcf104963f2ee227dd
fariz <farizi1906@gmail.com>
2022-07-09 13:38:35 +0700

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
     name="create-flask-project",
-    version="0.1.2-alpha",
+    version="0.2.1",
     description="a simple cli tools to generate template for flask app",
     long_description= open("README.md","r").read(),
     license="MIT",
@@ -13,13 +13,13 @@ setup(
     maintainer="https://github.com/AlphaBeta1906",
     py_modules=["main"],
     url="https://github.com/AlphaBeta1906/create-flask-app",
-    download_url="https://github.com/AlphaBeta1906/create-flask-app/archive/refs/tags/v0.1.1.tar.gz",
+    download_url="https://github.com/AlphaBeta1906/create-flask-app/archive/refs/tags/v0.2.1.tar.gz",
     entry_points="""
         [console_scripts]
         create-flask-app= cfa.main:create_flask_app
     """,
     classifiers=[
-        "Development Status :: 3 - Alpha"
+        "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Environment :: Console",
